74dd7dd0c5910d4e58c3bef6f053aec8d7afdf7a
shermondevelopment <victorshermon@yahoo.com>
2022-05-26 21:33:19 -0300

    chore: including prettier in app

--- app/index.js
+++ app/index.js
@@ -2,4 +2,6 @@ import express from 'express'
 
 const app = express()
 
-app.listen(process.env.PORT || '4000', () => console.log(`app running in port ${process.env.PORT} 🚀🚀🚀🚀`))
+app.listen(process.env.PORT || '4000', () =>
+  console.log(`app running in port ${process.env.PORT} 🚀🚀🚀🚀`)
+)
