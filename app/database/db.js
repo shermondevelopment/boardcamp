0b63d5eeebdd7a836d3023ce1b8e32bcb094930e
shermondevelopment <victorshermon@yahoo.com>
2022-05-26 21:58:34 -0300

    chore: connect database

---
+++ app/database/db.js
@@ -0,0 +1,13 @@
+import pg from 'pg'
+
+const { Pool } = pg
+
+const db = new Pool({
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  database: process.env.POSTGRES_DATABASE
+})
+
+export default db
--- app/index.js
+++ app/index.js
@@ -4,9 +4,18 @@ import 'dotenv/config'
 /** express */
 import express from 'express'
 
+/** database connect*/
+import db from './database/db.js'
+
 /* init server */
 const app = express()
 
-app.listen(process.env.PORT || '4000', () =>
-  console.log(`app running in port ${process.env.PORT} 🚀🚀🚀🚀`)
-)
+db.connect()
+  .then(() => {
+    app.listen(process.env.PORT || '4000', () =>
+      console.log(`app running in port ${process.env.PORT} 🚀🚀🚀🚀`)
+    )
+  })
+  .catch(() => {
+    console.log('opss connect how db failed 😵😵😵😵😵!')
+  })
