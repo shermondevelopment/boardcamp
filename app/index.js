ce809ee328a5fd9f0d51f41f692f2c5e57544b3d
shermondevelopment <victorshermon@yahoo.com>
2022-05-29 21:47:51 -0300

    remove connect of index.js

--- app/index.js
+++ app/index.js
@@ -7,9 +7,6 @@ import express, { json } from 'express'
 /** cors */
 import cors from 'cors'
 
-/** database connect*/
-import db from './database/db.js'
-
 /** routers */
 import routers from './routers/index.js'
 
@@ -19,12 +16,6 @@ app.use(json())
 app.use(cors())
 app.use(routers)
 
-db.connect()
-  .then(() => {
-    app.listen(process.env.PORT || '4000', () =>
-      console.log(`app running in port ${process.env.PORT} 🚀🚀🚀🚀`)
-    )
-  })
-  .catch(() => {
-    console.log('opss connect how db failed 😵😵😵😵😵!')
-  })
+app.listen(process.env.PORT || '4000', () =>
+  console.log(`app running in port ${process.env.PORT} 🚀🚀🚀🚀`)
+)
